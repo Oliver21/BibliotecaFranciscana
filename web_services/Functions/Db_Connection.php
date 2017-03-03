1dc260b929ee4c31e0b2135959c34b1a725a15ee
Luis Angel Martinez <lagwyf@gmail.com>
2017-03-03 00:42:25 -0600

    Conexión a la base de datos, agregar usuarios

--- web_services/Functions/Db_Connection.php
+++ web_services/Functions/Db_Connection.php
@@ -1,1 +1,37 @@
 <?php
+
+class Connection {
+    # Constructor
+    function __construct()
+    {
+    }
+
+    # Destructor
+    function __destruct()
+    {
+        // TODO: Implement __destruct() method.
+    }
+
+    # connect
+    public function connect(){
+        # Database and host values
+        $host = "mysql.hostinger.mx";
+        $user = "u340525141_bibfr";
+        $password = "biblioteca";
+        $database = "u340525141_bibfr";
+
+        # Try to connect to the database
+        $conn = mysqli_connect($host, $user, $password, $database);
+
+        # Set the charset to UTF-8
+        $conn->query("SET NAMES 'utf8");
+
+        # Return the connection to the functions file
+        return $conn;
+    }
+
+    # close
+    public function close(){
+        mysql_close();
+    }
+}
