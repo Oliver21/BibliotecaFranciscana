9ee44700ea68ea2cc36995e585b99f4cde7703b7
Luis Angel Martinez <lagwyf@gmail.com>
2017-04-14 02:29:50 -0500

    Folding de las funciones para PHPStorm

--- web_services/Functions/Data_Functions.php
+++ web_services/Functions/Data_Functions.php
@@ -5,7 +5,9 @@
  */
 class Functions
 {
-    # Global connection to the database
+
+
+    //<editor-fold desc="Class functions">
     /**
      * @var mysqli
      */
@@ -23,6 +25,8 @@ class Functions
         $this->db = $this->conn->connect();
     }
 
+
+
     # Destructor
     /**
      *
@@ -30,7 +34,9 @@ class Functions
     function __destruct()
     {
     }
+    //</editor-fold>
 
+    //<editor-fold desc="User functions">
     # Destroy a user session
     /**
      *
@@ -45,88 +51,115 @@ class Functions
         session_destroy();
     }
 
-    # Get the list of series
+    # Check if the user exist in the table
     /**
-     * @return array
+     * @param $Username
+     * @param $Contrasena
+     * @return array|mixed
      */
-    function get_series()
+    function check_user($Username, $Contrasena)
     {
-        # Query that selects all the series from the table
-        $query = "SELECT * FROM Serie";
-        if ($result = mysqli_query($this->db, $query)) {
-            # Check for the number of series
-            if ($result->num_rows > 0) {
-                $series = array();
-                while ($row = mysqli_fetch_row($result)) {
-                    $values = array("id_serie" => $row[0], "nombre_serie" => $row[1], "volumen_serie" => $row[2]);
-                    array_push($series, $values);
-                }
-                $series['status'] = 1;
-                return ($series);
-            } else {
-                # The authors table is empty
-                return (array("status" => 2, "message" => "No se han agregado series."));
-            }
-        } else {
-            # Something went wrong
-            return (array("status" => 0, "message" => "Algo salió mal obtener la información de las series."));
-        }
-    }
+        # Query that checks if the user is in the database
+        $query = "SELECT * FROM Usuario WHERE Username = '$Username' AND Contrasena = md5('$Contrasena')";
 
-    # Get the list of authors
-    /**
-     * @return array
-     */
-    function get_authors()
-    {
-        # Query that selects all the authors from the table
-        $query = "SELECT * FROM Autor";
+        # Execute query
         if ($result = mysqli_query($this->db, $query)) {
-            # Check for the number of magazines
-            if ($result->num_rows > 0) {
-                $authors = array();
-                while ($row = mysqli_fetch_assoc($result)) {
-                    array_push($authors, $row);
-                }
-                $authors['status'] = 1;
-                return ($authors);
+            if ($result->num_rows < 1) {
+                # Invalid credentials or user not exists
+                return (array("status" => 2, "message" => "No se ha encontrado usuario con estas credenciales."));
             } else {
-                # The authors table is empty
-                return (array("status" => 2, "message" => "No se han agregado autores."));
-            }
-        } else {
-            # Something went wrong
-            return (array("status" => 0, "message" => "Algo salió mal obtener la información de los autores."));
-        }
-    }
+                $results = $result->fetch_array(MYSQLI_ASSOC);
 
-    # Get all the magazines information
-    /**
-     * @return array
-     */
-    function get_magazines()
-    {
-        # Query that selects all the magazines from the table
-        $query = "SELECT * FROM Revista";
-        if ($result = mysqli_query($this->db, $query)) {
-            # Check for the number of magazines
-            if ($result->num_rows > 0) {
-                $magazines = array();
-                while ($row = mysqli_fetch_assoc($result)) {
-                    array_push($magazines, $row);
-                }
-                $magazines['status'] = 1;
-                return ($magazines);
-            } else {
-                # There are no magazines in the table
-                return (array("status" => 2, "message" => "No se han agregado revistas."));
+                $results['status'] = 1;
+                return ($results);
             }
         } else {
             # If something went wrong
-            return (array("status" => 0, "message" => "Algo salió mal obtener la información de las revistas."));
+            return (array("status" => 0, "message" => "Algo salió mal al buscar el usuario."));
         }
     }
 
+    # Add a user to the 'Usuario' table
+    /**
+     * @param $Nombre
+     * @param $Ap_Paterno
+     * @param $Ap_Materno
+     * @param $Username
+     * @param $Tipo_Usuario
+     * @param $Grado
+     * @param $Telefono
+     * @param $Correo
+     * @param $Direccion
+     * @param $Instituto_Proveniencia
+     * @param $Contrasena
+     * @return array
+     */
+    function add_user($Nombre, $Ap_Paterno, $Ap_Materno, $Username, $Tipo_Usuario, $Grado, $Telefono, $Correo, $Direccion, $Instituto_Proveniencia, $Contrasena)
+    {
+        # Check that the username does not exist
+        $query = "SELECT 1 FROM Usuario WHERE Username = '$Username'";
+        if ($result = mysqli_query($this->db, $query)) {
+            # Check the length of the answer
+            if ($result->num_rows > 0) {
+                # Username already exists
+                return (array("status" => 2, "message" => "El username ya existe."));
+            } else {
+                # Add the user
+                # Query to insert the new user to the database
+                $query = "INSERT INTO Usuario (Nombre, Ap_Paterno, Ap_Materno, Username, Tipo_Usuario, Grado, Telefono, Correo, Direccion, Instituto_Proveniencia, Contrasena) VALUES ('$Nombre', '$Ap_Paterno', '$Ap_Materno', '$Username', '$Tipo_Usuario', '$Grado', '$Telefono', '$Correo', '$Direccion', '$Instituto_Proveniencia', md5('$Contrasena'))";
+
+                # Execute the query
+                if ($result = mysqli_query($this->db, $query)) {
+                    # If the user was successfully added
+                    return (array("status" => 1, "message" => "Usuario añadido correctamente."));
+                } else {
+                    # If something went wrong
+                    return (array("status" => 0, "message" => "No es posible agregar al usuario."));
+                }
+            }
+        } else {
+            # If something went wrong
+            return (array("status" => 0, "message" => "Algo salió mal al validar el usuario."));
+        }
+    }
+
+
+
+    # Delete user
+    function delete_user($Username){
+        # Check that the user actually exists
+        $query_validate = "SELECT 1 FROM Usuario WHERE Username = '$Username'";
+
+        # Execute the validation query
+        if ($result = mysqli_query($this->db, $query_validate)){
+            # Check the number of results
+            if ($result->num_rows < 1 ){
+                # The username does not exists
+                return (array("status" => 100, "message" => "No existe el usuario a eliminar."));
+            } else {
+                # Delete the user from the database
+                # Query to eliminate the user
+                $query_delete = "DELETE FROM Usuario WHERE Username = '$Username'";
+
+                # Execute the deletion
+                if ( $result = mysqli_query($this->db, $query_delete) ){
+                    # Se borró el usuario
+                    return array("status" => 1, "message" => "Usuario eliminado exitosamente.");
+                } else {
+                    # If something went wrong
+                    return (array("status" => 101, "message" => "Existió un error al eliminar el usuario." . mysqli_error($this->db) ));
+                }
+
+            }
+
+        } else {
+            # If something went wrong
+            return (array("status" => 0, "message" => "Borrar usuario: Algo salió mal al validar el usuario."));
+        }
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="Book functions">
     # Get all the books information
     /**
      * @return array
@@ -179,110 +212,7 @@ class Functions
 
     }
 
-    # Add a user to the 'Usuario' table
-    /**
-     * @param $Nombre
-     * @param $Ap_Paterno
-     * @param $Ap_Materno
-     * @param $Username
-     * @param $Tipo_Usuario
-     * @param $Grado
-     * @param $Telefono
-     * @param $Correo
-     * @param $Direccion
-     * @param $Instituto_Proveniencia
-     * @param $Contrasena
-     * @return array
-     */
-    function add_user($Nombre, $Ap_Paterno, $Ap_Materno, $Username, $Tipo_Usuario, $Grado, $Telefono, $Correo, $Direccion, $Instituto_Proveniencia, $Contrasena)
-    {
-        # Check that the username does not exist
-        $query = "SELECT 1 FROM Usuario WHERE Username = '$Username'";
-        if ($result = mysqli_query($this->db, $query)) {
-            # Check the length of the answer
-            if ($result->num_rows > 0) {
-                # Username already exists
-                return (array("status" => 2, "message" => "El username ya existe."));
-            } else {
-                # Add the user
-                # Query to insert the new user to the database
-                $query = "INSERT INTO Usuario (Nombre, Ap_Paterno, Ap_Materno, Username, Tipo_Usuario, Grado, Telefono, Correo, Direccion, Instituto_Proveniencia, Contrasena) VALUES ('$Nombre', '$Ap_Paterno', '$Ap_Materno', '$Username', '$Tipo_Usuario', '$Grado', '$Telefono', '$Correo', '$Direccion', '$Instituto_Proveniencia', md5('$Contrasena'))";
 
-                # Execute the query
-                if ($result = mysqli_query($this->db, $query)) {
-                    # If the user was successfully added
-                    return (array("status" => 1, "message" => "Usuario añadido correctamente."));
-                } else {
-                    # If something went wrong
-                    return (array("status" => 0, "message" => "No es posible agregar al usuario."));
-                }
-            }
-        } else {
-            # If something went wrong
-            return (array("status" => 0, "message" => "Algo salió mal al validar el usuario."));
-        }
-    }
-
-    # Check if the user exist in the table
-    /**
-     * @param $Username
-     * @param $Contrasena
-     * @return array|mixed
-     */
-    function check_user($Username, $Contrasena)
-    {
-        # Query that checks if the user is in the database
-        $query = "SELECT * FROM Usuario WHERE Username = '$Username' AND Contrasena = md5('$Contrasena')";
-
-        # Execute query
-        if ($result = mysqli_query($this->db, $query)) {
-            if ($result->num_rows < 1) {
-                # Invalid credentials or user not exists
-                return (array("status" => 2, "message" => "No se ha encontrado usuario con estas credenciales."));
-            } else {
-                $results = $result->fetch_array(MYSQLI_ASSOC);
-
-                $results['status'] = 1;
-                return ($results);
-            }
-        } else {
-            # If something went wrong
-            return (array("status" => 0, "message" => "Algo salió mal al buscar el usuario."));
-        }
-    }
-
-    # Delete user
-    function delete_user($Username){
-        # Check that the user actually exists
-        $query_validate = "SELECT 1 FROM Usuario WHERE Username = '$Username'";
-
-        # Execute the validation query
-        if ($result = mysqli_query($this->db, $query_validate)){
-            # Check the number of results
-            if ($result->num_rows < 1 ){
-                # The username does not exists
-                return (array("status" => 100, "message" => "No existe el usuario a eliminar."));
-            } else {
-                # Delete the user from the database
-                # Query to eliminate the user
-                $query_delete = "DELETE FROM Usuario WHERE Username = '$Username'";
-
-                # Execute the deletion
-                if ( $result = mysqli_query($this->db, $query_delete) ){
-                    # Se borró el usuario
-                    return array("status" => 1, "message" => "Usuario eliminado exitosamente.");
-                } else {
-                    # If something went wrong
-                    return (array("status" => 101, "message" => "Existió un error al eliminar el usuario." . mysqli_error($this->db) ));
-                }
-
-            }
-
-        } else {
-            # If something went wrong
-            return (array("status" => 0, "message" => "Borrar usuario: Algo salió mal al validar el usuario."));
-        }
-    }
 
     # Add a book to the table
     /**
@@ -348,9 +278,99 @@ class Functions
             return (array("status" => 0, "message" => "Algo salió mal al validar el libro."));
         }
     }
+    //</editor-fold>
+
+    //<editor-fold desc="Serie functions">
+    # Get the list of series
+    /**
+     * @return array
+     */
+    function get_series()
+    {
+        # Query that selects all the series from the table
+        $query = "SELECT * FROM Serie";
+        if ($result = mysqli_query($this->db, $query)) {
+            # Check for the number of series
+            if ($result->num_rows > 0) {
+                $series = array();
+                while ($row = mysqli_fetch_row($result)) {
+                    $values = array("id_serie" => $row[0], "nombre_serie" => $row[1], "volumen_serie" => $row[2]);
+                    array_push($series, $values);
+                }
+                $series['status'] = 1;
+                return ($series);
+            } else {
+                # The authors table is empty
+                return (array("status" => 2, "message" => "No se han agregado series."));
+            }
+        } else {
+            # Something went wrong
+            return (array("status" => 0, "message" => "Algo salió mal obtener la información de las series."));
+        }
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="Author functions">
+    # Get the list of authors
+    /**
+     * @return array
+     */
+    function get_authors()
+    {
+        # Query that selects all the authors from the table
+        $query = "SELECT * FROM Autor";
+        if ($result = mysqli_query($this->db, $query)) {
+            # Check for the number of magazines
+            if ($result->num_rows > 0) {
+                $authors = array();
+                while ($row = mysqli_fetch_assoc($result)) {
+                    array_push($authors, $row);
+                }
+                $authors['status'] = 1;
+                return ($authors);
+            } else {
+                # The authors table is empty
+                return (array("status" => 2, "message" => "No se han agregado autores."));
+            }
+        } else {
+            # Something went wrong
+            return (array("status" => 0, "message" => "Algo salió mal obtener la información de los autores."));
+        }
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="Magazine functions">
+    # Get all the magazines information
+    /**
+     * @return array
+     */
+    function get_magazines()
+    {
+        # Query that selects all the magazines from the table
+        $query = "SELECT * FROM Revista";
+        if ($result = mysqli_query($this->db, $query)) {
+            # Check for the number of magazines
+            if ($result->num_rows > 0) {
+                $magazines = array();
+                while ($row = mysqli_fetch_assoc($result)) {
+                    array_push($magazines, $row);
+                }
+                $magazines['status'] = 1;
+                return ($magazines);
+            } else {
+                # There are no magazines in the table
+                return (array("status" => 2, "message" => "No se han agregado revistas."));
+            }
+        } else {
+            # If something went wrong
+            return (array("status" => 0, "message" => "Algo salió mal obtener la información de las revistas."));
+        }
+    }
+    //</editor-fold>
 
 }
 
+//<editor-fold desc="Tests">
 # Test de añadir usuario
 # $functions = new Functions();
 # $functions->add_user("Test04", "TestAP", "TestAM", "test04", "Admin", "0", "8111628532", "test01@test.com", "Test Address", "Tec", "test");
@@ -382,3 +402,4 @@ class Functions
 # Test de selección de libro específico
 # $functions = new Functions();
 # echo json_encode($functions->get_specific_book(2));
+//</editor-fold>
