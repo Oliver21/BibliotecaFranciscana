3bb7af934ae3770ef6f2fce68809930cf3f46186
Luis Angel Martinez <lagwyf@gmail.com>
2017-04-16 13:31:34 -0500

    Agregar las funciones para editar y eliminar libros

--- web_services/Books.php
+++ web_services/Books.php
@@ -25,7 +25,11 @@ if (isset($_POST['action'])){
             break;
         # Add book to the table
         case 4:
-            add_book();
+            add_book($functions);
+            break;
+        # Delete book
+        case 5:
+            delete_book($functions);
             break;
         default:
             echo json_encode( array("status" => 660, "message" => "Acción no válida.") );
@@ -59,12 +63,54 @@ function get_specific_book($functions){
     echo json_encode( $result ); 
 }
 
+/**
+ * @param $functions
+ */
+function delete_book($functions){
+    $id_libro = $_POST['id_libro'];
+    
+    echo json_encode( $functions->delete_book($id_libro) );
+}
+
 # edit_book
 /**
  * @param $functions
  */
 function edit_book($functions){
+    $id_libro = $_POST['id_libro'];
+    $isbn = $_POST['ISBN'];
+    $titulo_libro = $_POST['titulo_libro'];
+    $subtitulo_libro = $_POST['subtitulo_libro'];
+    $titulo_original = $_POST['titulo_original'];
+    $numero_paginas = $_POST['numero_paginas'];
+    $id_editorial = $_POST['id_editorial'];
+    $numero_edicion = $_POST['numero_edicion'];
+    $fecha_edicion = $_POST['fecha_edicion'];
+    $lugar_publicacion = $_POST['lugar_publicacion'];
+    $fecha_adquisicion = $_POST['fecha_adquisicion'];
+    $costo_libro = $_POST['costo_libro'];
+    $proveedor_libro = $_POST['proveedor_libro'];
+    $observaciones_libro = $_POST['observaciones_libro'];
+    $id_seccion = $_POST['id_seccion'];
+    $id_apartado = $_POST['id_apartado'];
+    $volumen_libro = $_POST['volumen_libro'];
+    $ilustraciones = $_POST['ilustraciones'];
+    $graficas = $_POST['graficas'];
+    $mapas = $_POST['mapas'];
+    $bibliografia = $_POST['bibliografia'];
+    $indice = $_POST['indice'];
+    $pasta_blanda = $_POST['pasta_blanda'];
+    $planos = $_POST['planos'];
+    $estatus = $_POST['estatus'];
+    $numero_copias = $_POST['numero_copias'];
+    $palabras_clave = $_POST['palabras_clave'];
 
+    $result = $functions->edit_book($id_libro, $isbn, $titulo_libro, $subtitulo_libro, $titulo_original, $numero_paginas, $id_editorial,
+        $numero_edicion, $fecha_edicion, $lugar_publicacion, $fecha_adquisicion, $costo_libro, $proveedor_libro, $observaciones_libro,
+        $id_seccion, $id_apartado, $volumen_libro, $ilustraciones, $graficas, $mapas, $bibliografia, $indice, $pasta_blanda, $planos,
+        $estatus, $numero_copias, $palabras_clave);
+
+    echo json_encode( $result );
 }
 
 # add_book
