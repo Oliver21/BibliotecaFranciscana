9828eec2f0a0e5b635ba9276a95343cf190d461f
Luis Angel Martinez <lagwyf@gmail.com>
2017-05-10 18:30:57 -0500

    Carga de opciones de autores

--- js/busqLibros.js
+++ js/busqLibros.js
@@ -4,7 +4,16 @@ $('#addLibroButton').click(function () {
             action: 1
         },
         function (returnedData) {
-            console.log(returnedData);
-            Materialize.toast(JSON.parse(returnedData).message, 4000);
+            console.log(JSON.parse(returnedData));
+            options = JSON.parse(returnedData);
+
+            var text = "";
+            for (var i = 0; i < options.length; i++){
+                text = "<option value=" + options[i].id_autor  +">"+ ( options[i].nombre_autor + " " +options[i].apaterno_autor ) + "</option>";
+                $('#autor_select').append(text);
+            }
+
+    //        "<option value="1">Option 1</option>";
+      //      Materialize.toast(JSON.parse(returnedData).message, 4000);
         });
 });
